085cc38ad5512c61207810be9a287bb34ea4e327
kawser <mkawserm@gmail.com>
2021-10-23 20:46:31 +0600

    tls support enabled

--- capability/httpserver2/httpserver2.go
+++ capability/httpserver2/httpserver2.go
@@ -3,6 +3,7 @@ package httpserver2
 import (
 	"context"
 	"errors"
+	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"github.com/mkawserm/abesh/constant"
 	"github.com/mkawserm/abesh/iface"
@@ -14,6 +15,7 @@ import (
 	"go.uber.org/zap"
 	"io/ioutil"
 	"net/http"
+	"sort"
 	"strings"
 	"time"
 )
@@ -27,8 +29,12 @@ type EventResponse struct {
 }
 
 type HTTPServer2 struct {
-	mHost                     string
-	mPort                     string
+	mHost string
+	mPort string
+
+	mCertFile string
+	mKeyFile  string
+
 	mRequestTimeout           time.Duration
 	mDefault404HandlerEnabled bool
 	mHandleMethodNotAllowed   bool
@@ -37,6 +43,17 @@ type HTTPServer2 struct {
 	mHttpServer       *http.Server
 	mHttpServerMux    *httprouter.Router
 	mEventTransmitter iface.IEventTransmitter
+
+	mDefaultContentType string
+
+	d401m string
+	d403m string
+	d404m string
+	d405m string
+	d408m string
+	d409m string
+	d499m string
+	d500m string
 }
 
 func (h *HTTPServer2) Name() string {
@@ -59,14 +76,61 @@ func (h *HTTPServer2) GetConfigMap() model.ConfigMap {
 	return h.mValues
 }
 
+func (h *HTTPServer2) buildDefaultMessage(code uint32) string {
+	return fmt.Sprintf(`
+		{
+			"code": "SE_%d",
+			"lang": "en",
+			"message": "%d ERROR",
+			"data": {}
+		}
+	`, code, code)
+}
+
 func (h *HTTPServer2) SetConfigMap(values model.ConfigMap) error {
 	h.mValues = values
 	h.mHost = values.String("host", "0.0.0.0")
 	h.mPort = values.String("port", "8080")
+
+	h.mCertFile = values.String("cert_file", "")
+	h.mKeyFile = values.String("key_file", "")
+
 	h.mRequestTimeout = values.Duration("default_request_timeout", time.Second)
+
 	h.mDefault404HandlerEnabled = values.Bool("default_404_handler_enabled", true)
 	h.mHandleMethodNotAllowed = values.Bool("handle_method_not_allowed", false)
+
+	h.mDefaultContentType = values.String("default_content_type", "application/json")
+
+	h.d401m = h.buildDefaultMessage(401)
+	h.d403m = h.buildDefaultMessage(403)
+	h.d404m = h.buildDefaultMessage(404)
+	h.d405m = h.buildDefaultMessage(405)
+	h.d408m = h.buildDefaultMessage(408)
+	h.d409m = h.buildDefaultMessage(409)
+	h.d499m = h.buildDefaultMessage(499)
+	h.d500m = h.buildDefaultMessage(500)
+
 	return nil
+}
+
+func (h *HTTPServer2) getMessage(key, defaultValue, lang string) string {
+	data := h.mValues.String(fmt.Sprintf("%s_%s", key, lang), "")
+
+	if len(data) == 0 {
+		data = h.mValues.String(key, defaultValue)
+	}
+
+	return data
+}
+
+func (h *HTTPServer2) getLanguage(r *http.Request) string {
+	l := r.Header.Get("Accept-Language")
+	if len(l) == 0 {
+		l = "en"
+	}
+
+	return l
 }
 
 func (h *HTTPServer2) SetEventTransmitter(eventTransmitter iface.IEventTransmitter) error {
@@ -131,7 +195,7 @@ func (h *HTTPServer2) Setup() error {
 				elapsed := time.Since(timerStart)
 				logger.L(h.ContractId()).Debug("request execution time", zap.Duration("seconds", elapsed))
 			}()
-			h.s404m(writer, nil)
+			h.s404m(request, writer, nil)
 			return
 		}
 
@@ -156,7 +220,7 @@ func (h *HTTPServer2) Setup() error {
 			logger.L(h.ContractId()).Debug("request execution time", zap.Duration("seconds", elapsed))
 		}()
 
-		h.s500m(writer, nil)
+		h.s500m(request, writer, nil)
 		return
 	}
 
@@ -171,8 +235,15 @@ func (h *HTTPServer2) Setup() error {
 
 func (h *HTTPServer2) Start(_ context.Context) error {
 	logger.L(h.ContractId()).Info("http server started at " + h.mHttpServer.Addr)
-	if err := h.mHttpServer.ListenAndServe(); err != http.ErrServerClosed {
-		return err
+
+	if len(h.mCertFile) != 0 && len(h.mKeyFile) != 0 {
+		if err := h.mHttpServer.ListenAndServeTLS(h.mCertFile, h.mKeyFile); err != http.ErrServerClosed {
+			return err
+		}
+	} else {
+		if err := h.mHttpServer.ListenAndServe(); err != http.ErrServerClosed {
+			return err
+		}
 	}
 
 	return nil
@@ -186,7 +257,7 @@ func (h *HTTPServer2) Stop(ctx context.Context) error {
 	return nil
 }
 
-func (h *HTTPServer2) s403m(writer http.ResponseWriter, errLocal error) {
+func (h *HTTPServer2) writeMessage(statusCode int, defaultMessage string, request *http.Request, writer http.ResponseWriter, errLocal error) {
 	if errLocal != nil {
 		logger.L(h.ContractId()).Error(errLocal.Error(),
 			zap.String("version", h.Version()),
@@ -194,9 +265,9 @@ func (h *HTTPServer2) s403m(writer http.ResponseWriter, errLocal error) {
 			zap.String("contract_id", h.ContractId()))
 	}
 
-	writer.Header().Add("Content-Type", h.mValues.String("default_content_type", "application/text"))
-	writer.WriteHeader(http.StatusForbidden)
-	if _, err := writer.Write([]byte(h.mValues.String("s403m", "403 ERROR"))); err != nil {
+	writer.Header().Add("Content-Type", h.mDefaultContentType)
+	writer.WriteHeader(statusCode)
+	if _, err := writer.Write([]byte(h.getMessage(fmt.Sprintf("s%dm", statusCode), defaultMessage, h.getLanguage(request)))); err != nil {
 		logger.L(h.ContractId()).Error(err.Error(),
 			zap.String("version", h.Version()),
 			zap.String("name", h.Name()),
@@ -204,95 +275,32 @@ func (h *HTTPServer2) s403m(writer http.ResponseWriter, errLocal error) {
 	}
 }
 
-func (h *HTTPServer2) s404m(writer http.ResponseWriter, errLocal error) {
-	if errLocal != nil {
-		logger.L(h.ContractId()).Error(errLocal.Error(),
-			zap.String("version", h.Version()),
-			zap.String("name", h.Name()),
-			zap.String("contract_id", h.ContractId()))
-	}
-
-	writer.Header().Add("Content-Type", h.mValues.String("default_content_type", "application/text"))
-	writer.WriteHeader(http.StatusNotFound)
-	if _, err := writer.Write([]byte(h.mValues.String("s404m", "404 ERROR"))); err != nil {
-		logger.L(h.ContractId()).Error(err.Error(),
-			zap.String("version", h.Version()),
-			zap.String("name", h.Name()),
-			zap.String("contract_id", h.ContractId()))
-	}
+func (h *HTTPServer2) s401m(request *http.Request, writer http.ResponseWriter, errLocal error) {
+	h.writeMessage(401, h.d401m, request, writer, errLocal)
 }
 
-func (h *HTTPServer2) s405m(writer http.ResponseWriter, errLocal error) {
-	if errLocal != nil {
-		logger.L(h.ContractId()).Error(errLocal.Error(),
-			zap.String("version", h.Version()),
-			zap.String("name", h.Name()),
-			zap.String("contract_id", h.ContractId()))
-	}
-
-	writer.Header().Add("Content-Type", h.mValues.String("default_content_type", "application/text"))
-	writer.WriteHeader(http.StatusMethodNotAllowed)
-	if _, err := writer.Write([]byte(h.mValues.String("s405m", "405 ERROR"))); err != nil {
-		logger.L(h.ContractId()).Error(err.Error(),
-			zap.String("version", h.Version()),
-			zap.String("name", h.Name()),
-			zap.String("contract_id", h.ContractId()))
-	}
+func (h *HTTPServer2) s403m(request *http.Request, writer http.ResponseWriter, errLocal error) {
+	h.writeMessage(403, h.d403m, request, writer, errLocal)
 }
 
-func (h *HTTPServer2) s408m(writer http.ResponseWriter, errLocal error) {
-	if errLocal != nil {
-		logger.L(h.ContractId()).Error(errLocal.Error(),
-			zap.String("version", h.Version()),
-			zap.String("name", h.Name()),
-			zap.String("contract_id", h.ContractId()))
-	}
-
-	writer.Header().Add("Content-Type", h.mValues.String("default_content_type", "application/text"))
-	writer.WriteHeader(http.StatusRequestTimeout)
-	if _, err := writer.Write([]byte(h.mValues.String("s408m", "408 ERROR"))); err != nil {
-		logger.L(h.ContractId()).Error(err.Error(),
-			zap.String("version", h.Version()),
-			zap.String("name", h.Name()),
-			zap.String("contract_id", h.ContractId()))
-	}
+func (h *HTTPServer2) s404m(request *http.Request, writer http.ResponseWriter, errLocal error) {
+	h.writeMessage(404, h.d404m, request, writer, errLocal)
 }
 
-func (h *HTTPServer2) s499m(writer http.ResponseWriter, errLocal error) {
-	if errLocal != nil {
-		logger.L(h.ContractId()).Error(errLocal.Error(),
-			zap.String("version", h.Version()),
-			zap.String("name", h.Name()),
-			zap.String("contract_id", h.ContractId()))
-	}
-
-	writer.Header().Add("Content-Type", h.mValues.String("default_content_type", "application/text"))
-	writer.WriteHeader(499)
-
-	if _, err := writer.Write([]byte(h.mValues.String("s499m", "499 ERROR"))); err != nil {
-		logger.L(h.ContractId()).Error(err.Error(),
-			zap.String("version", h.Version()),
-			zap.String("name", h.Name()),
-			zap.String("contract_id", h.ContractId()))
-	}
+func (h *HTTPServer2) s405m(request *http.Request, writer http.ResponseWriter, errLocal error) {
+	h.writeMessage(405, h.d405m, request, writer, errLocal)
 }
 
-func (h *HTTPServer2) s500m(writer http.ResponseWriter, errLocal error) {
-	if errLocal != nil {
-		logger.L(h.ContractId()).Error(errLocal.Error(),
-			zap.String("version", h.Version()),
-			zap.String("name", h.Name()),
-			zap.String("contract_id", h.ContractId()))
-	}
+func (h *HTTPServer2) s408m(request *http.Request, writer http.ResponseWriter, errLocal error) {
+	h.writeMessage(408, h.d408m, request, writer, errLocal)
+}
 
-	writer.Header().Add("Content-Type", h.mValues.String("default_content_type", "application/text"))
-	writer.WriteHeader(http.StatusInternalServerError)
-	if _, err := writer.Write([]byte(h.mValues.String("s500m", "500 ERROR"))); err != nil {
-		logger.L(h.ContractId()).Error(err.Error(),
-			zap.String("version", h.Version()),
-			zap.String("name", h.Name()),
-			zap.String("contract_id", h.ContractId()))
-	}
+func (h *HTTPServer2) s499m(request *http.Request, writer http.ResponseWriter, errLocal error) {
+	h.writeMessage(499, h.d499m, request, writer, errLocal)
+}
+
+func (h *HTTPServer2) s500m(request *http.Request, writer http.ResponseWriter, errLocal error) {
+	h.writeMessage(500, h.d500m, request, writer, errLocal)
 }
 
 func (h *HTTPServer2) debugMessage(request *http.Request) {
@@ -310,19 +318,24 @@ func (h *HTTPServer2) AddService(
 	triggerValues model.ConfigMap,
 	service iface.IService) error {
 
-	var method string
+	var methodString string
 	var path string
+	var methodList []string
 
-	if method = triggerValues.String("method", ""); len(method) == 0 {
+	// url http access method
+	if methodString = triggerValues.String("method", ""); len(methodString) == 0 {
 		return ErrMethodNotDefined
 	}
+	methodString = strings.ToUpper(strings.TrimSpace(methodString))
+	methodList = strings.Split(methodString, ",")
+	if len(methodList) > 0 {
+		sort.Strings(methodList)
+	}
 
-	method = strings.ToUpper(strings.TrimSpace(method))
-
+	// url http path
 	if path = triggerValues.String("path", ""); len(path) == 0 {
 		return ErrPathNotDefined
 	}
-
 	path = strings.TrimSpace(path)
 
 	requestHandler := func(writer http.ResponseWriter, request *http.Request) {
@@ -338,8 +351,8 @@ func (h *HTTPServer2) AddService(
 
 		h.debugMessage(request)
 
-		if method != request.Method {
-			h.s405m(writer, nil)
+		if !utility.IsIn(methodList, request.Method) {
+			h.s405m(request, writer, nil)
 			return
 		}
 
@@ -376,13 +389,13 @@ func (h *HTTPServer2) AddService(
 
 		if authorizer != nil {
 			if !authorizer.IsAuthorized(authorizerExpression, metadata) {
-				h.s403m(writer, nil)
+				h.s403m(request, writer, nil)
 				return
 			}
 		}
 
 		if data, err = ioutil.ReadAll(request.Body); err != nil {
-			h.s500m(writer, err)
+			h.s500m(request, writer, err)
 			return
 		}
 
@@ -416,21 +429,21 @@ func (h *HTTPServer2) AddService(
 
 		select {
 		case <-nCtx.Done():
-			h.s408m(writer, nil)
+			h.s408m(request, writer, nil)
 			return
 		case r := <-ch:
 			if r.Error == context.DeadlineExceeded {
-				h.s408m(writer, r.Error)
+				h.s408m(request, writer, r.Error)
 				return
 			}
 
 			if r.Error == context.Canceled {
-				h.s499m(writer, r.Error)
+				h.s499m(request, writer, r.Error)
 				return
 			}
 
 			if r.Error != nil {
-				h.s500m(writer, r.Error)
+				h.s500m(request, writer, r.Error)
 				return
 			}
 
@@ -453,7 +466,9 @@ func (h *HTTPServer2) AddService(
 		}
 	}
 
-	h.mHttpServerMux.HandlerFunc(method, path, requestHandler)
+	for _, method := range methodList {
+		h.mHttpServerMux.HandlerFunc(method, path, requestHandler)
+	}
 
 	return nil
 }
